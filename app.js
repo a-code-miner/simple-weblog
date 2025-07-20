91d419bc455109eb50ae6e852f44158c571a0dc4
MortazaAkhlaqi <akhlaqimortaza1@gmail.com>
2025-07-20 11:33:11 +0430

    55 config.env setted and cross-env and NODE_ENV for production and development mode

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 
 import express from 'express'
+import dotenv from 'dotenv'
 
 import router from './routes/index.js'
 
 // تبدیل import.meta.url به __dirname
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+
+//* Load Config
+dotenv.config({ path: './configs/config.env' })
 
 const app = express()
 
@@ -21,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 //* Routes
 app.use(router)
 
+const PORT = process.env.PORT || 5000
 
-
-app.listen(3000, () => { console.log(`Server is running on localhost:3000`) })
+app.listen(PORT, () => {
+    console.log(`Server is running in ${process.env.NODE_ENV} mode on localhost:${PORT}`)
+})
