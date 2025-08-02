56c5d69eb679ad16988c03818ed24b1b34a6de5c
MortazaAkhlaqi <akhlaqimortaza1@gmail.com>
2025-08-02 17:31:30 +0430

    66 Yup added for form validating

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,14 @@
 import { Router } from "express";
+import Yup from 'yup'
 
 const router = Router()
+
+const schema = Yup.object().shape({
+    fullname: Yup.string().required('نام کامل الزامی است.').min(3, 'نام کامل باید حداقل ۳ کاراکتر باشد.').max(50, 'نام کامل نباید بیشتر از ۵۰ کاراکتر باشد.'),
+    email: Yup.string().email('ایمیل نامعتبر است.').required('ایمیل الزامی است.'),
+    password: Yup.string().required('رمز عبور الزامی است.').min(6, 'رمز عبور باید حداقل ۶ کاراکتر باشد.').max(20, 'رمز عبور نباید بیشتر از ۲۰ کاراکتر باشد.'),
+    confirmPassword: Yup.string().oneOf([Yup.ref('password'), null], 'رمز عبور و تأیید رمز عبور باید یکسان باشند.').required('تأیید رمز عبور الزامی است.')
+})
 
 // @desc    Login Page
 // @route    GET /users/login
@@ -17,8 +25,32 @@ router.get('/register', (req, res) => {
 // @desc    Handle User Registration
 // @route    POST /users/register
 router.post('/register', (req, res) => {
-    console.log(req.body)
-    res.send('User registration logic goes here')
+    const validator = schema.validate(req.body)
+    console.log(validator)
+    validator
+        .then((result) => {
+            console.log(result)
+            res.send('Validation passed, proceed with registration logic')
+        })
+        .catch(err => {
+            console.log(err.errors)
+            res.send({
+                errors: err.errors
+            })
+        })
+
+    // did not work for me for some reason
+    // schema.validate(req.body)
+    //     .then((result) => {
+    //         console.log(result)
+    //         res.send('Validation passed, proceed with registration logic')
+    //     })
+    //     .catch((err) => {
+    //         console.log(err.errors)
+    //         res.send({
+    //             errors: err.errors
+    //         })
+    //     })
 })
 
 export default router
