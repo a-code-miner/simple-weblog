48bcb807adba9d8686a6b7b30985de3c213173ee
MortazaAkhlaqi <akhlaqimortaza1@gmail.com>
2025-08-05 14:00:23 +0430

    73 now when user registers, user data will save to db

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,23 +9,61 @@ export const register = async (req, res) => {
 }
 
 export const createUser = async (req, res) => {
+    const errors = []
     try {
         await User.validateUser(req.body);
-        // await User.create(req.body)
+        const { fullname, email, password } = req.body
+
+        const existingUser = await User.find({ email })
+
+        if (existingUser.length > 0) {
+            errors.push({
+                message: 'این ایمیل قبلا ثبت‌نام شده است.',
+            })
+            return res.render('register', {
+                pageTitle: 'ثبت‌نام کاربر جدید',
+                path: '/register',
+                errors,
+                oldData: req.body
+            })
+        }
+        await User.create(req.body)
         res.redirect('/users/login')
+        // const user = new User({
+        //     fullname,
+        //     email,
+        //     password
+        // })
+        // user.save()
+        //     .then((user) => {
+        //         console.log('User created successfully:', user);
+        //         res.redirect('/users/login')
+        //     })
+        //     .catch((err) => {
+        //         if (err) throw err
+        //     })
     } catch (err) {
         console.log(err)
-        const errors = []
-        err.inner.forEach(error => {
-            errors.push({
-                name: error.path,
-                message: error.message
+        if (err.inner && Array.isArray(err.inner)) {
+            err.inner.forEach(error => {
+                errors.push({
+                    name: error.path,
+                    message: error.message
+                })
             })
-        })
+        } else {
+            // اگر inner وجود ندارد، شاید خطا از جای دیگری آمده
+            errors.push({
+                name: 'general',
+                message: err.message || 'خطای ناشناخته‌ای رخ داده است.'
+            })
+        }
+
         return res.render('register', {
             pageTitle: 'ثبت‌نام کاربر جدید',
             path: '/register',
             errors,
+            oldData: req.body
         })
     }
 }
