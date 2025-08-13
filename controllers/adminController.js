51221a7b171a9c5c6305fd6e5cb20e3c8fece10f
MortazaAkhlaqi <akhlaqimortaza1@gmail.com>
2025-08-13 16:29:54 +0430

    87 dashboard seperated and adminController added

---
+++ controllers/adminController.js
@@ -0,0 +1,8 @@
+export const getDashboard = async (req, res) => {
+    res.render('private/blogs', {
+        pageTitle: 'بخش مدیریت | داشبورد',
+        path: '/dashboard',
+        layout: './layouts/dashLayout',
+        fullname: req.user.fullname,
+    })
+}
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import isAuthenticated from "../middlewares/auth.js";
 
+import { getDashboard } from "../controllers/adminController.js";
+
 const router = new Router()
 
 // @desc    Dashboard Home Page
 // @route   GET /dashboard
-router.get('/', isAuthenticated, (req, res) => {
-    res.render('dashboard', { pageTitle: 'بخش مدیریت | داشبورد', path: '/dashboard', layout: './layouts/dashLayout', fullname: req.user.fullname })
-})
+router.get('/', isAuthenticated, getDashboard)
 
 export default router
